b9ca728ce0afd3704e218b8d926b7fc8e6f20a00
Elijah Blair <elijahblair2005@gmail.com>
2023-12-10 11:48:58 -0500

    Completed Exercise

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿string charName;
+int age;
+char gender;
+bool hasRecord;
+double yearlyIncome;
+decimal stockPercentage;
+
+charName = "Bruce Banks";
+age = 24;
+gender = 'M';
+hasRecord = false;
+yearlyIncome = 4760000;
+stockPercentage = 1.9m;
+
+Console.WriteLine($"Your name is {charName}");
+Console.WriteLine($"You are {age}");
+Console.WriteLine($"Your gender is {gender}");
+Console.WriteLine($"You have a criminal record: {hasRecord}");
+Console.WriteLine($"You currently make {yearlyIncome} a year");
+Console.WriteLine($"You currently own {stockPercentage}% of your companies stock");
+
